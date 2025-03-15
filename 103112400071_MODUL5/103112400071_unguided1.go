b7d3057fc41a1c25a48d27f86f786f649c80443b
Raihan <raihanadi073@gmail.com>
2025-03-15 10:00:00 +0700

    raihan

--- 103112400071_MODUL5/103112400071_unguided1.go
+++ 103112400071_MODUL5/103112400071_unguided1.go
@@ -4,35 +4,26 @@ package main
 
 import "fmt"
 
-func factorial(n int) int {
-	result := 1
-	for i := 1; i <= n; i++ {
-		result *= i
+func deretFibonacci(x int) int {
+	if x == 0 {
+		return 0
 	}
-	return result
-}
-func permutation(n, r int) int {
-	return factorial(n) / factorial(n-r)
-}
-func combination(n, r int) int {
-	return factorial(n) / (factorial(r) * factorial(n-r))
-}
-func processValues(a, b, c, d int) {
-	if a >= c && b >= d {
-		// Menghitung permutasi dan kombinasi
-		perm1 := permutation(a, c)
-		comb1 := combination(a, c)
-		perm2 := permutation(b, d)
-		comb2 := combination(b, d)
-		//hasil
-		fmt.Println(perm1, comb1)
-		fmt.Println(perm2, comb2)
-	} else {
-		fmt.Println("input tidak sesuai")
+	if x == 1 {
+		return 1
 	}
+	return deretFibonacci(x-1) + deretFibonacci(x-2)
 }
+
 func main() {
-	var a, b, c, d int
-	fmt.Scan(&a, &b, &c, &d)
-	processValues(a, b, c, d)
+	var n int
+	fmt.Scan(&n)
+
+	// Cetak deret Fibonacci
+	for i := 0; i <= n; i++ {
+		fmt.Print(deretFibonacci(i))
+		if i < n {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Println()
 }
--- 103112400071_MODUL5/103112400071_unguided3.go
+++ 103112400071_MODUL5/103112400071_unguided3.go
@@ -1,33 +1,20 @@
-//RAIHAN ADI ARBA
-//103112400071
-
+// Raihan Adi Arba
+// 103112400071
 package main
 
 import "fmt"
 
-func CetakDeret(n int, hasil *float64) {
-	fmt.Print(n, " ")
-
-	var ntemp int = n
-	for ntemp != 1 {
-		if ntemp%2 == 0 {
-			*hasil = float64(ntemp) / 2
-			ntemp = int(*hasil)
-		} else {
-			*hasil = 3*float64(ntemp) + 1
-			ntemp = int(*hasil)
+func faktorBilangan(x, i int) {
+	if i <= x {
+		if x%i == 0 {
+			fmt.Print(i, " ")
 		}
-		fmt.Print(ntemp, " ")
+		faktorBilangan(x, i+1)
 	}
 }
 
 func main() {
-	var n int
-	var deret float64
-	fmt.Scan(&n)
-	if n < 1000000 {
-		CetakDeret(n, &deret)
-	} else {
-		fmt.Print("Eror")
-	}
+	var N int
+	fmt.Scan(&N)
+	faktorBilangan(N, 1)
 }
